02439c9ee4aae92eac8d7fe7a6df36240c4d5313
krumaska <51515055+krumaska@users.noreply.github.com>
2019-12-31 05:30:07 +0900

    Update main.dart to use hot reload

--- Flutter/i_am_rich/lib/main.dart
+++ Flutter/i_am_rich/lib/main.dart
@@ -1,11 +1,19 @@
 import 'package:flutter/material.dart';
 
+//The main function is the starting point for all our Flutter app
 void main() {
   runApp(
-    MaterialApp(
-      home: Center(
-        child: Text('Hello World'),
-      ),
-    ),
+    MyApp()
   );
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Center(
+        child: Text('w'),
+      ),
+    );
+  }
 }

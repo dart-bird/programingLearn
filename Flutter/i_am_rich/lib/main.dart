0b5fc1cbb2a270d72493ef161d26877a4d76a022
krumaska <51515055+krumaska@users.noreply.github.com>
2019-12-31 06:27:10 +0900

    Update main.dart include Using Scaffold

--- Flutter/i_am_rich/lib/main.dart
+++ Flutter/i_am_rich/lib/main.dart
@@ -11,9 +11,18 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Center(
-        child: Text('w'),
-      ),
+      home: Scaffold(
+        backgroundColor: Colors.blueGrey,
+        appBar: AppBar(
+          title: Text('I Am Rich'),
+          backgroundColor: Colors.blueGrey[900],
+        ),
+        body: Center(
+          child: Image(
+            image: NetworkImage('http://www.bricon.co.kr/shop/data/goods/1541962004958l0.png'),
+          ),
+        ),
+      )
     );
   }
 }

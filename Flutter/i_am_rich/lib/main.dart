66c6b4e6dad4149e8b634de4f974d11832af11cb
krumaska <51515055+krumaska@users.noreply.github.com>
2020-01-06 20:46:59 +0900

    Update main.dart reformat

--- Flutter/i_am_rich/lib/main.dart
+++ Flutter/i_am_rich/lib/main.dart
@@ -2,9 +2,7 @@ import 'package:flutter/material.dart';
 
 //The main function is the starting point for all our Flutter app
 void main() {
-  runApp(
-    MyApp()
-  );
+  runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
@@ -14,15 +12,13 @@ class MyApp extends StatelessWidget {
       home: Scaffold(
         backgroundColor: Colors.blueGrey,
         appBar: AppBar(
-          title: Text('I Am Rich'),
-          backgroundColor: Colors.blueGrey[900],
+          title: Text('I Am Rich!'),
+          backgroundColor: Colors.blueGrey[130],
         ),
         body: Center(
-          child: Image(
-            image: AssetImage('images/diamond.png')
-            ),
+          child: Image(image: AssetImage('images/diamond.png')),
         ),
-      )
+      ),
     );
   }
 }

8c908f5d9ba9ed74ea229903cc773faef6171014
krumaska <51515055+krumaska@users.noreply.github.com>
2020-01-12 16:00:40 +0900

    Update main.dart changes struct && include refresh Button

--- Flutter/dicee-flutter/lib/main.dart
+++ Flutter/dicee-flutter/lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'dart:math';
 
 void main() {
   return runApp(
@@ -15,26 +16,62 @@ void main() {
   );
 }
 
-class DicePage extends StatelessWidget {
+class DicePage extends StatefulWidget {
+  @override
+  _DicePageState createState() => _DicePageState();
+}
+
+class _DicePageState extends State<DicePage> {
+  int leftDiceNumber = 5;
+  int rightDiceNumber = 5;
+  void diceAction() {
+    setState(() {
+      rightDiceNumber = Random().nextInt(6) + 1;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Center(
-      child: Row(
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
         children: <Widget>[
-          Expanded(
-            child: FlatButton(
-              onPressed: () {
-                print('Left Button got Pressed');
-              },
-              child: Image.asset('images/dice1.png'),
-            ),
+          Row(
+            children: <Widget>[
+              Expanded(
+                child: FlatButton(
+                  onPressed: () {
+                    diceAction();
+                  },
+                  child: Image.asset('images/dice$leftDiceNumber.png'),
+                ),
+              ),
+              Expanded(
+                child: FlatButton(
+                  onPressed: () {
+                    diceAction();
+                  },
+                  child: Image.asset('images/dice$rightDiceNumber.png'),
+                ),
+              ),
+            ],
           ),
-          Expanded(
-            child: FlatButton(
-              onPressed: () {
-                print('Right Button got Pressed');
-              },
-              child: Image.asset('images/dice2.png'),
+          SizedBox(
+            height: 50.0,
+          ),
+          FlatButton(
+            color: Colors.white,
+            padding: EdgeInsets.all(10.0),
+            onPressed: () {
+              setState(() {
+                leftDiceNumber = Random().nextInt(6) + 1;
+                rightDiceNumber = Random().nextInt(6) + 1;
+              });
+            },
+            child: Icon(
+              Icons.refresh,
+              size: 80.0,
+              color: Colors.red,
             ),
           ),
         ],

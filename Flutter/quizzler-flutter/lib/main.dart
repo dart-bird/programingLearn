31ac1b183bb156957971f090d8f9f36594ae3d7d
krumaska <51515055+krumaska@users.noreply.github.com>
2020-01-18 17:20:44 +0900

    Update main.dart

--- Flutter/quizzler-flutter/lib/main.dart
+++ Flutter/quizzler-flutter/lib/main.dart
@@ -30,11 +30,7 @@ class QuizPage extends StatefulWidget {
 class _QuizPageState extends State<QuizPage> {
   int questionsCnt = 0;
   List<Icon> scoreKeeper = [];
-
   
-
-  int questionNumber = 0;
-
   @override
   Widget build(BuildContext context) {
     return Column(
@@ -47,7 +43,7 @@ class _QuizPageState extends State<QuizPage> {
             padding: EdgeInsets.all(10.0),
             child: Center(
               child: Text(
-                quizBrain.questionBank[questionNumber].questionText,
+                quizBrain.getQuestionText(),
                 textAlign: TextAlign.center,
                 style: TextStyle(
                   fontSize: 25.0,
@@ -73,16 +69,15 @@ class _QuizPageState extends State<QuizPage> {
               onPressed: () {
                 //The user picked true.
                 bool correctAnswer =
-                  quizBrain.questionBank[questionNumber].questionAnswer;
+                  quizBrain.getQuestionBool();
                 if (correctAnswer == true) {
                   print('user got it right!');
                 } else {
                   print('user got it wrong');
                 }
                 setState(() {
-                  questionNumber++;
+                  quizBrain.nextQuestion();
                 });
-                print(questionNumber);
               },
             ),
           ),
@@ -102,16 +97,15 @@ class _QuizPageState extends State<QuizPage> {
               onPressed: () {
                 //The user picked false.
                 bool correctAnswer =
-                    quizBrain.questionBank[questionNumber].questionAnswer;
+                    quizBrain.getQuestionBool();
                 if (correctAnswer == false) {
                   print('user got it right!');
                 } else {
                   print('user got it wrong');
                 }
                 setState(() {
-                  questionNumber++;
+                  quizBrain.nextQuestion();
                 });
-                print(questionNumber);
               },
             ),
           ),

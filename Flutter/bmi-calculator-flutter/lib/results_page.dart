9fe452329dfa26a39883562e3c57463432dc3687
krumaska <51515055+krumaska@users.noreply.github.com>
2020-01-26 18:43:41 +0900

    Create results_page.dart

---
+++ Flutter/bmi-calculator-flutter/lib/results_page.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class ResultsPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('BMI 계산기'),
+      ),
+      body: Text('Hello'),
+    );
+  }
+}

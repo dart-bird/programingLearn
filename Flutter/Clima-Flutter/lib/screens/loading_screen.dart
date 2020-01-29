c61f8dacae96df0c98e246b1c8fc843cf44e1058
krumaska <51515055+krumaska@users.noreply.github.com>
2020-01-30 03:17:34 +0900

    Update loding_screen.dart change to navigator with locationData

--- Flutter/Clima-Flutter/lib/screens/loading_screen.dart
+++ Flutter/Clima-Flutter/lib/screens/loading_screen.dart
@@ -1,7 +1,8 @@
-import 'package:clima/services/location.dart';
+import 'package:clima/services/weather.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_spinkit/flutter_spinkit.dart';
 
-import 'http.dart';
+import 'location_screen.dart';
 
 class LoadingScreen extends StatefulWidget {
   @override
@@ -9,18 +10,23 @@ class LoadingScreen extends StatefulWidget {
 }
 
 class _LoadingScreenState extends State<LoadingScreen> {
+  double latitude;
+  double longitude;
   @override
   void initState() {
     // TODO: implement initState
     super.initState();
-    getLocation();
+    getLocationData();
   }
 
-  void getLocation() async {
-    Location location = Location();
-    await location.getCurrentLocation();
-    print(location.latitude);
-    print(location.longtitude);
+  void getLocationData() async {
+    WeatherModel weatherModel = WeatherModel();
+    var weatherData = await weatherModel.getLocationWeather();
+    Navigator.push(context, MaterialPageRoute(builder: (context) {
+      return LocationScreen(
+        locationWeather: weatherData,
+      );
+    }));
   }
 
   @override
@@ -33,8 +39,11 @@ class _LoadingScreenState extends State<LoadingScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text('wow'),
+      body: Center(
+        child: SpinKitChasingDots(
+          color: Colors.white,
+          size: 100.0,
+        ),
       ),
     );
   }

be57a981ff1a7d65fe74b839be693b535220a0b6
krumaska <51515055+krumaska@users.noreply.github.com>
2020-01-30 00:32:27 +0900

    Update loading_screen.dart include init screen get location

--- Flutter/Clima-Flutter/lib/screens/loading_screen.dart
+++ Flutter/Clima-Flutter/lib/screens/loading_screen.dart
@@ -1,4 +1,7 @@
+import 'package:clima/services/location.dart';
 import 'package:flutter/material.dart';
+
+import 'http.dart';
 
 class LoadingScreen extends StatefulWidget {
   @override
@@ -7,15 +10,31 @@ class LoadingScreen extends StatefulWidget {
 
 class _LoadingScreenState extends State<LoadingScreen> {
   @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    getLocation();
+  }
+
+  void getLocation() async {
+    Location location = Location();
+    await location.getCurrentLocation();
+    print(location.latitude);
+    print(location.longtitude);
+  }
+
+  @override
+  void deactivate() {
+    // TODO: implement deactivate
+    super.deactivate();
+    print('deactivate');
+  }
+
+  @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: RaisedButton(
-          onPressed: () {
-            //Get the current location
-          },
-          child: Text('Get Location'),
-        ),
+      appBar: AppBar(
+        title: Text('wow'),
       ),
     );
   }

c848f7108ff07e7f870562b38a4cdedb7bc64999
krumaska <51515055+krumaska@users.noreply.github.com>
2020-02-27 16:48:35 +0900

    Update rounded_button.dart include TextStyle

--- Flutter/flash-chat-flutter/lib/components/rounded_button.dart
+++ Flutter/flash-chat-flutter/lib/components/rounded_button.dart
@@ -19,6 +19,10 @@ class RoundedButton extends StatelessWidget {
           height: 42.0,
           child: Text(
             title,
+            style: TextStyle(
+              color: Colors.white,
+              fontWeight: FontWeight.bold,
+            ),
           ),
         ),
       ),
